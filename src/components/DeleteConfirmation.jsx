80e2f8ecff2f4b60d1672be41e094e9359249287
Jayme Desrosiers <40217128+fatal-zio@users.noreply.github.com>
2025-01-03 17:29:57 -0600

    Added progress bar to Modal

--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,15 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+const TIMEOUT = 3000;
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  const [remainingTime, setRemainingTime] = useState(TIMEOUT);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setRemainingTime((previousTime) => previousTime - 10);
+    }, 10);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   useEffect(() => {
     const timer = setTimeout(() => {
       onConfirm();
-    }, 3000);
+    }, TIMEOUT);
 
     return () => {
       clearTimeout(timer);
     };
-  }, []);
+  }, [onConfirm]);
 
   return (
     <div id="delete-confirmation">
@@ -23,6 +37,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
+      <progress value={remainingTime} max={TIMEOUT} />
     </div>
   );
 }
